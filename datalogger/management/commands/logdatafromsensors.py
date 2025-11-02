26525a787d691baad42638264fa54c38f6741b20
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-11-02 14:50:02 +0100

    Refactor `logdatafromsensors` command to streamline sensor data reading and measurement creation

--- datalogger/management/commands/logdatafromsensors.py
+++ datalogger/management/commands/logdatafromsensors.py
@@ -41,3 +41,52 @@ class Command(BaseCommand):
                 data.append(new_measure)
 
         Measure.objects.bulk_create(data)
+
+
+
+# from typing import Callable, Dict, Tuple
+#
+# from django.conf import settings
+# from django.core.management.base import BaseCommand
+#
+# from m5stack import QMP6988, SHT30, QMP6988Fake, SHT30Fake
+# from datalogger.models import Sensor, Measure
+#
+# # Extract constant: kódy senzorů
+# SHT30_TEMP_CODE = "M5STACK-ENV-3-UNIT-SHT30-TEMPERATURE"
+# SHT30_HUMIDITY_CODE = "M5STACK-ENV-3-UNIT-SHT30-HUMIDITY"
+# QMP6988_PRESSURE_CODE = "M5STACK-ENV-3-UNIT-QMP6988-PRESSURE"
+#
+#
+# class Command(BaseCommand):
+#     help = "Načte měření z M5Stack ENV III (SHT30, QMP6988) a uloží je do Measure."
+#
+#     def _get_drivers(self) -> Tuple[QMP6988 | QMP6988Fake, SHT30 | SHT30Fake]:
+#         """
+#         Vybere reálné nebo falešné drivery podle settings.SENSORS_ENABLED.
+#         """
+#         if getattr(settings, "SENSORS_ENABLED", False):
+#             return QMP6988(), SHT30()
+#         return QMP6988Fake(), SHT30Fake()
+#
+#     def handle(self, *args, **options):
+#         qmp6988, sht30 = self._get_drivers()
+#
+#         # Mapování kódu senzoru na funkci pro čtení hodnoty
+#         readers: Dict[str, Callable[[], float]] = {
+#             SHT30_TEMP_CODE: sht30.read_temperature,
+#             SHT30_HUMIDITY_CODE: sht30.read_humidity,
+#             QMP6988_PRESSURE_CODE: qmp6988.read_pressure,
+#         }
+#
+#         # Načti pouze relevantní senzory a jen potřebná pole
+#         sensors = Sensor.objects.filter(code__in=readers.keys()).only("id", "code")
+#
+#         # Vytvoř seznam měření bez duplicitní logiky if/if/if
+#         measures_to_create = [
+#             Measure(sensor=sensor, value=readers[sensor.code]())
+#             for sensor in sensors
+#         ]
+#
+#         if measures_to_create:
+#             Measure.objects.bulk_create(measures_to_create)
